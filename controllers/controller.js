e659565c0f85af5285d1172d9063a5c8b3e11d36
Iasmhim <Iasmhimvilacahp@gmail.com>
2025-08-13 20:08:06 -0300

    Casos

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,5 +14,20 @@ const denuncias =[
     data: "2022-01",
     descricao: "Servidor público matou a pauladas o cão Costela. Condenado a 6 anos, 3 meses e 18 dias de reclusão."
   },
-  
+
+  {
+    caso: "Nina",
+    especie: "Gata",
+    local: "Rio de Janeiro - RJ",
+    data: "2019-08",
+    descricao: "Gata pisou no pé de uma funcionária de segurança que, em seguida, a matou com violência. Autora foi presa e multada; supermercado também recebeu multa milionária."
+  },
+
+  {
+    caso: "Búfalos de Brotas",
+    especie: "Búfalos e cavalos",
+    local: "Brotas - SP",
+    data: "2021-11",
+    descricao: "Mais de 1.000 búfalos e dezenas de cavalos foram encontrados sem comida e água em fazenda. Proprietário preso; penalidades podem somar centenas de anos de prisão e milhões em multas."
+  },
 ]
