19526e089914e84d3e6d9ae48a1debb5e433cb54
Iasmhim <Iasmhimvilacahp@gmail.com>
2025-08-13 20:06:28 -0300

    Casos

---
+++ controllers/controller.js
@@ -0,0 +1,18 @@
+const denuncias =[
+  {
+    caso: "Manchinha",
+    especie: "Cachorro (SRD)",
+    local: "Osasco - SP",
+    data: "2018-11",
+    descricao: "Cachorro abandonado em estacionamento de hipermercado Carrefour foi envenenado e espancado por segurança. Morreu pouco depois. Caso teve grande repercussão e levou à criação da Lei 14.064/2020."
+  },
+
+  {
+    caso: "Costela",
+    especie: "Cachorro (Buldogue)",
+    local: "Rio Grande - RS",
+    data: "2022-01",
+    descricao: "Servidor público matou a pauladas o cão Costela. Condenado a 6 anos, 3 meses e 18 dias de reclusão."
+  },
+  
+]
