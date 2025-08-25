aa8f8c8da48b7a6037ad6f0c5c171f5ca5a9cd34
JaquelineAbreu-code <abreujaqueline61@gmail.com>
2025-08-25 18:20:39 -0300

    Adicionando rotas

---
+++ routes/route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const { criarDenuncia, listarDenuncia, deletarDenuncia, editarDenuncia} = require('../controllers/controller.js');
+
+router.post('/criar-denuncia', criarDenuncia);
+
+router.get('/listar-denuncia', listarDenuncia);
+
+router.delete('/deletar-denuncia', deletarDenuncia);
+
+router.put('/editar-denuncia', editarDenuncia);
+
+// método put, método delete
+
+module.exports = router;
