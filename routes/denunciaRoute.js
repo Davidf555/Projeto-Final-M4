2bc3b049e80c167055dd3028dc9cefc3cf022abd
Davidf555 <daviflavio555@gmail.com>
2025-08-25 22:07:54 -0300

    correção

--- routes/denunciaRoute.js
+++ routes/denunciaRoute.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD:routes/denunciaRoute.js
-const { criarDenuncia, listarDenuncia, deletarDenuncia, editarDenuncia} = require('../controllers/denunciaController.js');
-=======
-const { criarDenuncia, listarDenuncias, deletarDenuncia, editarDenuncia} = require('../controllers/controller.js');
+
+const { criarDenuncia, listarDenuncias, deletarDenuncia, editarDenuncia} = require('../controllers/denunciaController.js');
 
 const verificarDenuncia = require('../middlewares/midlle.js')
->>>>>>> 91f7a7dffa6a3a2324feadcee817fdb58334a2c6:routes/route.js
 
-router.post('/criar-denuncia', criarDenuncia);
+router.post('/criar-denuncia', verificarDenuncia, criarDenuncia);
 
 router.get('/listar-denuncia', listarDenuncias);
 
